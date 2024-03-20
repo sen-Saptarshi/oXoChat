1a117ae3a20acdc7c731e1a398a167e72ee0610d
Saptarshi Sen <113275417+sen-Saptarshi@users.noreply.github.com>
2024-03-20 18:34:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+
+const messages = [];
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.json()); // <==== parse request body as JSON
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.emit("messages", messages);
+  socket.on("chat message", (msg) => {
+    messages.push(msg);
+    io.emit("chat message", msg);
+    // console.log(msg)
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
